d130be5212f3f1caed4a40a57dd8bc5f5b24a5bf
Dirk Eddelbuettel <edd@debian.org>
2016-01-04 21:57:34 -0600

    more fixes to help file and structure
    now free of WARNINGS or NOTES

--- R/data.R
+++ R/data.R
@@ -15,20 +15,23 @@
 #'
 #' It was converted from the original PDF format to CSV using Tabula.
 #'
+#' @docType data
+#' @name alldata
 #' @format A \sQuote{tbl_df} and data frame object with 11480 observation of 34 variables
 #' @source \url{https://www.fbi.gov/about-us/cjis/nics/reports/nics_firearm_checks_-_month_year_by_state_type.pdf}
 #' @seealso The GitHub repository created by Buzzfeed containing a similar data set
 #' at \url{https://github.com/BuzzFeedNews/nics-firearm-background-checks/}
-"alldata"
+NULL
 
 #' US Population Growth data
 #'
 #' Montly observations about population growth
 #'
 #' @docType data
+#' @name poptotal
 #' @format A \sQuote{tbl_df} and data frame object with 217 observation of 3 variables
 #' @source US Census Bureau 
-"poptotal"
+NULL
 
 
 
